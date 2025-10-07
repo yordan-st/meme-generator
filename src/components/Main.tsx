f11f6d896757b864b86fbb73621deec00eb08a92
Yordan <yordan.stoyanov@mendix.com>
2025-10-07 15:16:53 +0200

    feat: implement meme fetching and random selection

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,17 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+interface Meme {
+  id: string;
+  name: string;
+  url: string;
+  width: number;
+  height: number;
+  box_count: number;
+}
+
+interface MemeData {
+  topText: string;
+  bottomText: string;
+  imageUrl: string;
+}
 
 export default function Main() {
-  const [memeData, setmemeData] = useState({
+  const [memesArray, setMemesArray] = useState<Meme[]>([]);
+
+  const [memeData, setmemeData] = useState<MemeData>({
     topText: "One does not simply",
     bottomText: "Walk into Mordor",
     imageUrl: "http://i.imgflip.com/1bij.jpg",
   });
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setMemesArray(data.data.memes));
+  }, []);
 
   function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     const { name, value } = event.target;
     setmemeData((prevMemeData) => ({
       ...prevMemeData,
       [name]: value,
+    }));
+  }
+
+  function getMemeImage() {
+    if (memesArray.length === 0) return;
+
+    const randomIndex = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomIndex].url;
+    setmemeData((prevMemeData) => ({
+      ...prevMemeData,
+      imageUrl: url,
     }));
   }
 
@@ -39,7 +73,7 @@ export default function Main() {
             value={memeData.bottomText}
           />
         </label>
-        <button>Get a new meme image 🖼</button>
+        <button onClick={getMemeImage}>Get a new meme image 🖼</button>
       </div>
       <div className="meme">
         <img src={memeData.imageUrl} />
